77e9a11dddef2fef45aee1a75baedc4c0ad800b5
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2022-06-13 19:03:42 +0300

    Create UserStore class

---
+++ src/pm.dal/UsersStore.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include"pch.h"
+
+#include"../pm.consoleApp/Database.h"
+#include"../../lib/nanodbc/nanodbc.h"
+
+namespace pm::dal
+{
+	class UsersStore
+	{
+	public:
+
+		struct USER
+		{
+			int id;
+			std::string firstName;
+			std::string lastName;
+			std::string email;
+			short age;
+			std::string password;
+			nanodbc::date createdOn;
+			short admin;
+		};
+
+		bool createUser(USER& user);
+		bool updateUser(size_t id, USER& user);
+		bool removeUser(size_t id);
+
+		USER getUserByEmail(std::string& email);
+		USER getUserById(unsigned id);
+
+	private:
+
+		DatabaseManagement db;
+
+	};
+}
