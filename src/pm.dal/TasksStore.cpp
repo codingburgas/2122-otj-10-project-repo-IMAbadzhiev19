aea6a709e3f4c2e4f448a00d73d9cd4d2775b84b
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2022-06-21 21:18:00 +0300

    Finish tasks functionallity in data access layer

--- src/pm.dal/TasksStore.cpp
+++ src/pm.dal/TasksStore.cpp
@@ -47,3 +47,52 @@ void pm::dal::TasksStore::removeTaskFromUser(size_t taskId, size_t userId)
 	std::string query = "DELETE FROM [TasksAndUsers] WHERE UserId = " + std::to_string(userId) + " AND TaskId = " + std::to_string(taskId);
 	db.executeQuery(query);
 }
+
+std::vector<pm::dal::TasksStore::TASK> pm::dal::TasksStore::loadAllTasks()
+{
+	std::vector<TASK> tasks;
+	std::string query = "SELECT * FROM Tasks";
+
+	nanodbc::result res = db.getResultFromSelect(query);
+
+	while (res.next())
+	{
+		tasks.emplace_back(res.get<int>(0), res.get<std::string>(1), res.get<std::string>(2), res.get<std::string>(3), res.get<nanodbc::date>(4), res.get<int>(5));
+	}
+
+	return tasks;
+}
+
+std::vector<pm::dal::ProjectsStore::PROJECT> pm::dal::TasksStore::getProjectsFromTask(size_t taskId)
+{
+	std::vector<pm::dal::ProjectsStore::PROJECT> projects;
+
+	std::string query = "SELECT Projects.Id, Projects.Title, Projects.Description, Projects.CreatedOn, Projects.CreatorId "
+		"FROM Projects, Tasks, ProjectsAndTasks WHERE (Projects.Id = ProjectId) AND (Tasks.Id = TaskId) AND (TaskId = " + std::to_string(taskId) + ")";
+
+	nanodbc::result res = db.getResultFromSelect(query);
+
+	while (res.next())
+	{
+		projects.emplace_back(res.get<int>(0), res.get<std::string>(1), res.get<std::string>(2), res.get<nanodbc::date>(3), res.get<int>(4));
+	}
+
+	return projects;
+}
+
+std::vector<pm::dal::UsersStore::USER> pm::dal::TasksStore::getUsersFromTask(size_t taskId)
+{
+	std::vector<pm::dal::UsersStore::USER> users;
+
+	std::string query = "SELECT Users.Id, Users.FirstName, Users.LastName, Users.Email, Users.Age, Users.Password, Users.CreatedOn, Users.Admin "
+		"FROM Users, Tasks, TasksAndUsers WHERE (Users.Id = UserId) AND (Tasks.Id = TaskId) AND (TaskId = " + std::to_string(taskId) + ")";
+
+	nanodbc::result res = db.getResultFromSelect(query);
+
+	while (res.next())
+	{
+		users.emplace_back(res.get<int>(0), res.get<std::string>(1), res.get<std::string>(2), res.get<std::string>(3), res.get<int>(4), res.get<std::string>(5), res.get<nanodbc::date>(6), res.get<short>(7));
+	}
+
+	return users;
+}
