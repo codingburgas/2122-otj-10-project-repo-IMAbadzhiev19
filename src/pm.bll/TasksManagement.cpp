f5f73ba95ed6266fd8446130f609014a40170d24
Ivaylo Abadzhiev <IMAbadzhiev19@codingburgas.bg>
2022-06-21 21:28:32 +0300

    Add TasksManagement structure - bll

--- src/pm.bll/TasksManagement.cpp
+++ src/pm.bll/TasksManagement.cpp
@@ -1,1 +1,38 @@
 #include "pch.h"
+
+#include "TasksManagement.h"
+
+void pm::bll::TasksManagement::createTask(pm::dal::TasksStore::TASK& task, size_t userId)
+{
+	m_tasksStore.createTask(task, userId);
+}
+
+void pm::bll::TasksManagement::updateTask(pm::dal::TasksStore::TASK& task, size_t taskId, size_t userId)
+{
+	m_tasksStore.updateTask(task, taskId, userId);
+}
+
+void pm::bll::TasksManagement::deleteTask(size_t taskId)
+{
+	m_tasksStore.deleteTask(taskId);
+}
+
+void pm::bll::TasksManagement::assignProjectToTask(size_t projectId, size_t taskId)
+{
+	m_tasksStore.assignProjectToTask(projectId, taskId);
+}
+
+void pm::bll::TasksManagement::removeProjectFromTask(size_t projectId, size_t taskId)
+{
+	m_tasksStore.removeProjectFromTask(projectId, taskId);
+}
+
+void pm::bll::TasksManagement::assignTaskToUser(size_t taskId, size_t userId)
+{
+	m_tasksStore.assignTaskToUser(taskId, userId);
+}
+
+void pm::bll::TasksManagement::removeTaskFromUser(size_t taskId, size_t userId)
+{
+	m_tasksStore.removeTaskFromUser(taskId, userId);
+}
--- src/pm.bll/TasksManagement.h
+++ src/pm.bll/TasksManagement.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include "pch.h"
+
+#include "../../lib/nanodbc/nanodbc.h"
+#include "../pm.consoleApp/Database.h"
+
+#include "../pm.dal/TasksStore.h"
+#include "../pm.bll/ProjectsManagement.h"
+#include "../pm.bll/TeamsManagement.h"
+#include "../pm.bll/UsersManagement.h"
+
+namespace pm::bll
+{
+	class TasksManagement
+	{
+	public:
+
+		pm::dal::TasksStore m_tasksStore;
+
+		void createTask(pm::dal::TasksStore::TASK& task, size_t userId);
+		void updateTask(pm::dal::TasksStore::TASK& task, size_t taskId, size_t userId);
+		void deleteTask(size_t taskId);
+
+		void assignProjectToTask(size_t projectId, size_t taskId);
+		void removeProjectFromTask(size_t projectId, size_t taskId);
+
+		void assignTaskToUser(size_t taskId, size_t userId);
+		void removeTaskFromUser(size_t taskId, size_t userId);
+
+		std::vector<pm::dal::TasksStore::TASK> loadAllTasks();
+		std::vector<pm::dal::ProjectsStore::PROJECT> getProjectsFromTask(size_t taskId);
+		std::vector<pm::dal::UsersStore::USER> getUsersFromTask(size_t taskId);
+
+	};
+}
